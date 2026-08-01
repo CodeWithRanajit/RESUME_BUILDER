605b5a46af204fd11707c23891b69a63e62b7677
Ranajit Chattaraj <ranajitchattaraj2004@gmail.com>
2026-08-01 12:47:46 +0530

    added new mode into the backend

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,5 +1,45 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true,
+        minlength: [3, "Name must be at least 3 characters long"],
+        maxlength:[50, "Name cannot exceed 50 characters"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        trim: true,
+        unique: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please enter a valid email"],
+        index:true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength:[8,"Password must be at least 8 characters long"],
+    },
+    isEmailVerified: {
+        type: Boolean,
+        default: false,
+    },
+    emailVerifiedAt: {
+        type: Date,
+        default:null
+    },
+    lastLogin: {
+        type: Date,
+        default: null,
+    },
+    refreshToken: {
+        type: String,
+        default:null,
+    },
     
-},{timestamps:true});
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+export default User;
