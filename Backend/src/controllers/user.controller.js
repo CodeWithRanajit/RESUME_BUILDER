6bc4c7a48bffe21632dbea466ccc84344bc6eb4d
CodeWithRanajit <ranajitchattaraj2004@gmail.com>
2026-08-23 13:33:55 +0530

    added resenddemailotp controller

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -176,33 +176,94 @@ export const login = asyncHandler(async (req, res) => {
       emailVerificationOtpModel.otp = otpForEmailVerifiaction;
       await emailVerificationOtpModel.save();
     }
-    return res.status(200).json(
-      new ApiResponse(200, null ,"Email verification required. A new OTP has been sent to your email")
-    )
-  }
-    user.lastLogin=Date.now();
-    await user.save();
-    const loggedInUser= await User.findById(user._id).select("-password -refreshToken");
-
-    const {accessToken,refreshToken}=await generateAccessTokenAndRefreshToken(user._id);
-
-    const accessTokenOptions = {
-      httpOnly: true,
-      secure: true,
-      sameSite: "strict",
-      maxAge: 15 * 60 * 1000,
-    };
-
-    const refreshTokenOptions = {
-      httpOnly: true,
-      secure: true,
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    };
     return res
       .status(200)
-      .cookie("accessToken", accessToken, accessTokenOptions)
-      .cookie("refreshToken", refreshToken, refreshTokenOptions)
-      .json(new ApiResponse(200, loggedInUser , "User logged In Successfully"));
+      .json(
+        new ApiResponse(
+          200,
+          null,
+          "Email verification required. A new OTP has been sent to your email",
+        ),
+      );
+  }
+  user.lastLogin = Date.now();
+  await user.save();
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken",
+  );
 
+  const { accessToken, refreshToken } =
+    await generateAccessTokenAndRefreshToken(user._id);
+
+  const accessTokenOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 15 * 60 * 1000,
+  };
+
+  const refreshTokenOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  };
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, accessTokenOptions)
+    .cookie("refreshToken", refreshToken, refreshTokenOptions)
+    .json(new ApiResponse(200, loggedInUser, "User logged In Successfully"));
+});
+export const resendVerifyEmailOtp = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    throw new ApiError(400, "Email not found");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ApiError(401, "Account not found. Please sign up first.");
+  }
+   if (!user.isEmailVerified) {
+    const createdUserFirstName = capitalizeName(user.name);
+    const otpForEmailVerifiaction =
+      await emailVerificationModel.generateOtpForEmailVerification();
+    const htmlemialTemplateForVerifyEmail = emailTemplateForEmailVerification
+      .replace("{{name}}", createdUserFirstName)
+      .replace("{{otp}}", otpForEmailVerifiaction);
+
+    await sendEmail(
+      user.email,
+      "Verify Your Email Adress — ResumeCraft",
+      htmlemialTemplateForVerifyEmail,
+    );
+
+    const emailVerificationOtpModel = await emailVerificationModel.findOne({
+      userId: user._id,
+    });
+
+    if (!emailVerificationOtpModel) {
+      await emailVerificationModel.create({
+        userId: user._id,
+        email: user.email,
+        otp: otpForEmailVerifiaction,
+      });
+    } else {
+      emailVerificationOtpModel.otp = otpForEmailVerifiaction;
+      await emailVerificationOtpModel.save();
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          null,
+          "A new OTP has been sent to your email. Please use it to verify your account."
+        ),
+      );
+  }
+  else{
+    return res.status(409).json(
+     new ApiResponse(409, null, "Your email is already verified. Please log in to continue.")
+    )
+  }
 });
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { login, registerUser } from "../controllers/user.controller.js";
+import { login, registerUser, resendVerifyEmailOtp } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login",login);
+router.post("/resend-verify-email-otp",resendVerifyEmailOtp);
 
 export default router;
